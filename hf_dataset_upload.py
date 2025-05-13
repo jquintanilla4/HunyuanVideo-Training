023fc492e39407bc8610b35edf371e3655250c33
Jorge Quintanilla <jquintanilla4@gmail.com>
2025-05-13 17:31:29 +0900

    feat: use .env file for Hugging Face repo ID configuration

--- hf_dataset_upload.py
+++ hf_dataset_upload.py
@@ -1,8 +1,12 @@
 from huggingface_hub import HfApi
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # Load environment variables from .env file
 api = HfApi()
 
 api.upload_large_folder(
-    repo_id="xxx/xxx", # Change to your own repo ID
+    repo_id=os.getenv("REPO_ID"), # Change to your own repo ID
     repo_type="dataset", # Change to either a model or dataset, depending on your use case
     folder_path="data/small", # Change to your own folder path
 )
